5d75e07f64fa2185fd8ac7574f7a47dc87d41782
Chris Hosegood <chris.hosegood@gmail.com>
2013-09-14 14:30:46 +0100

    Corrected jshint warnings

--- src/problem1.js
+++ src/problem1.js
@@ -1,15 +1,18 @@
-"use strict";
+/*global define*/
 
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
 define(function () {
+    "use strict";
+
     var problem1 = function (upperBound) {
         var result = 0,
             i = 1;
 
-        while (i < upperBound) {
+        for (; i < upperBound; i++) {
             if (i % 3 === 0 || i % 5 === 0) {
                 result = result + i;
             }
-            i++;
         }
 
         return result;
--- test/problem1Spec.js
+++ test/problem1Spec.js
@@ -1,14 +1,20 @@
-"use strict";
+/*global define, describe, it, expect*/
 
 //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 //Find the sum of all the multiples of 3 or 5 below 1000.
 define(['problem1'], function(problem1) {
+    "use strict";
 
     describe('problem1: Multiples of 3 and 5', function() {
 
         it('should return the sum of all multiples of 3 and 5 for all natural numbers below 10 ', function() {
             var result = problem1(10);
             expect(result).toEqual(23);
+        });
+
+        it('should return the sum of all multiples of 3 and 5 for all natural numbers below 1000', function() {
+            var result = problem1(1000);
+            expect(result).toEqual(233168);
         });
 
     });
